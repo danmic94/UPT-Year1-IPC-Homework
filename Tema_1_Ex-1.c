97f5ae47f0fe88c05591ba27bec340562920cc3a
Dan Nicolae Mic <micdan1994@gmail.com>
2017-12-05 21:15:55 +0200

    Initial commit exercises 1 and 2

---
+++ Tema_1_Ex-1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+  void main(void) {
+    float a, b, c, b1, d, delta;
+
+    printf("Introduceti coeficientii ecuatiei de gradul 2 mai jos pe rand.\n");
+    printf("Coeficientul a este: ");
+    scanf("%f", &a);
+    printf("\nCoeficientul b este: ");
+    scanf("%f", &b);
+    printf("\nCoeficientul c este: ");
+    scanf("%f", &c);
+
+    b1 = -b / (2 * a);
+    delta = pow(b1,2.00) - c / a;
+
+    if (delta == 0) {
+      printf("Solutie dubla: %f\n", b1);
+    }
+    else if (delta > 0) {
+      d = sqrt(delta);
+      printf("Solutii: %.1f si %.1f\n", b1 + d, b1 - d);
+    } else /* cand delta este mai mic decat zero*/{
+      d = sqrt(-delta);
+      printf("Solutii: %.1f+%.1fi si %.1f-%.1fi\n", b1, d, b1, d);
+    }
+  }
---
+++ tema1(ex2).c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main()
+{
+    float x;
+    int option;
+    bool infinite_loop;
+
+    infinite_loop = true;
+
+    while ( infinite_loop ){
+        printf("\n Va rog alegeti opţiunea dorita din meniu: \n");
+        printf("1 - ex \n");
+        printf("2 - ln(x) \n");
+        printf("3 - |x| \n");
+        printf("4 - 10x \n");
+        printf("0 - Ieşire \n \n \n ");
+
+        printf("\t Introduceti optiunea dorita: \n");
+        scanf("\t %d",&option);
+
+        if(option == 0 || option < 0 || option > 4){
+            printf("0 - Ieşire \n \n \n ");
+            break;
+        }
+
+        printf("\t Numarul care o sa fie calculat: \n");
+        scanf("\t %f",&x);
+
+        switch(option) {
+            /* Exponentiala lui x*/
+           case 1:
+            printf("1 - ex = %.02f \n", exp(x));
+            continue;
+           /* Logaritm natural a lui x*/
+           case 2:
+            if(x > 0){
+              printf("2 - ln x = %.02f \n", log(x));
+              continue;
+            }
+            printf("2 - ln x = Introduceti un x pentru care opertiunea de logaritm natural e calculabila");
+            continue;
+           /* Modulul lui x*/
+           case 3:
+            printf("3 - |x| = %d \n", (int)abs(x));
+            continue;
+           /* 10 inmultit cu x*/
+           case 4:
+            printf("4 - 10x = %.02f \n", 10*x);
+            continue;
+        }
+    }
+}
