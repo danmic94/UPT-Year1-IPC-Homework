e715d5f610c9ab01d5f9a3d26c046c545d9c210a
Dan Nicolae Mic <micdan1994@gmail.com>
2018-02-07 17:30:29 +0200

    Solved exercise 2

--- tema2(ex2).c
+++ tema2(ex2).c
@@ -2,28 +2,57 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*Se citesc de la tastatură elementele unei matrice.
+ Să se afişeze matricea şi să se numere câte dintre elementele acesteia sunt mai mari decât media lor aritmetică.*/
 void main()
 {
-    /** Using this as reference
-    int a[3][4] = {
-    {0, 1, 2, 3} ,     initializers for row indexed by 0
-    {4, 5, 6, 7} ,     initializers for row indexed by 1
-    {8, 9, 10, 11}     initializers for row indexed by 2
-   **/
-   int rows = 0,columns = 0;
+    int rows = 0,columns = 0,current_number = 0;
 
-   do(){
-       printf("Please insert the number of rows in the 2d array : ");
-       scanf("%d",&rows);
-       printf("\n");
-       printf("Please insert the number of columns in the 2d array : ");
-       scanf("%d",&columns);
-   }while(rows > 1 && coulmns > 1)
+    do
+    {
+        printf("Please insert the number of rows in the 2d array : ");
+        scanf("%d",&rows);
+        printf("Please insert the number of columns in the 2d array : ");
+        scanf("%d",&columns);
+        printf("\n");
+    }
+    while(rows < 1 && columns < 1);
 
-   int matrix_array[rows][columns];
+    int matrix_array[rows][columns];
+    int number_count = rows * columns;
+    int numbers_total = 0, higher_than_avg_count = 0;
+    int i,j,row_counter = 0,column_counter = 0 ,average;
 
-   //@TODO for to populate table
-   //@TODO inside calculate the avreage of the numbers
-   //@TODO itterate again and find how many numbers are above average
+    for(i=0; i<rows; i++)
+    {
+        row_counter++;
+        for(j=0; j<columns; j++)
+        {
+            column_counter++;
+            printf("Please insert the number located in %d row on %d colum : ",row_counter,column_counter);
+            scanf("%d",&current_number);
+            matrix_array[i][j] = current_number;
+            numbers_total += current_number;
+        }
+    }
 
+    average = numbers_total / number_count;
+
+
+    for(i=0; i<rows; i++)
+    {
+        for(j=0; j<columns; j++)
+        {
+            printf("%3d",matrix_array[i][j]);
+            if(matrix_array[i][j]>average)
+            {
+                higher_than_avg_count++;
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Tere are %d numbers higher than the average being %d!",higher_than_avg_count,average);
+
+    exit(0);
 }
